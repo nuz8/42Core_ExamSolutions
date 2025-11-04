50556abfc1a6fb02176bda9f19509d59b97c21f0
pamatya <pamatya@student.42heilbronn.de>
2025-11-04 20:34:28 +0100

    Advancing the bigInt ex

--- BigInt/BigInt.hpp
+++ BigInt/BigInt.hpp
@@ -3,12 +3,14 @@
 
 # include <iostream>
 # include <sstream>
-
+# include <climits>
 
 class BigInt
 {
 private:
 	std::string	_value;
+	long long	_cto;		// carry the one
+	std::string	_remVal;
 public:	
 	BigInt();
 	BigInt(const long long num);
@@ -20,8 +22,11 @@ public:
 	void		setValue(const long long num);
 	void		setValue(const std::string & num);
 	std::string	getValue() const;
+	size_t		getLlmaxLen();
+	std::string	toString(const long long n);
+	long long	toLlong(const std::string num);
 
-	// BigInt		operator+(const BigInt & rhs);
+	BigInt		operator+(const BigInt & rhs);
 	// BigInt		operator+=(const BigInt & rhs);
 	// BigInt		operator+=(const int rhs);
 
